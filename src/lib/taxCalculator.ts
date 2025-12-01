9a9126c10158218134e005468ff608a12b381135
xrendan <brendan@yuma.ai>
2025-12-01 10:01:38 -0700

    Fix health premium tax rate

--- src/lib/taxCalculator.ts
+++ src/lib/taxCalculator.ts
@@ -101,34 +101,34 @@ export function calculateOntarioTax(income: number): number {
 
 function calculateOntarioSurtax(basetax: number): number {
   if (basetax <= 5710) {
-        return 0;
-    } else if (basetax > 5710 && basetax <= 7307) {
-        return 0.20 * (basetax - 5710);
-    } else {
-        const surtaxAbove5710 = 0.20 * (7307 - 5710);
-        const surtaxAbove7307 = 0.36 * (basetax - 7307);
-        return surtaxAbove5710 + surtaxAbove7307;
-    }
+    return 0;
+  } else if (basetax <= 7307) {
+    return 0.2 * (basetax - 5710);
+  } else {
+    const surtaxAbove5710 = 0.2 * (7307 - 5710);
+    const surtaxAbove7307 = 0.36 * (basetax - 7307);
+    return surtaxAbove5710 + surtaxAbove7307;
+  }
 }
 
 function calculateOntarioHealthPm(income: number): number {
-    if (income <= 20000) {
-        return 0;
-    } else if (income > 20000 && income <= 36000) {
-        return Math.min(300, 0.06 * (income - 20000));
-    } else if (income > 36000 && income <= 48000) {
-        const taxAbove36000 = 0.06 * (income - 36000);
-        return Math.min(450, 300 + taxAbove36000);
-    } else if (income > 48000 && income <= 72000) {
-        const taxAbove48000 = 0.25 * (income - 48000);
-        return Math.min(600, 450 + taxAbove48000);
-    } else if (income > 72000 && income <= 200000) {
-        const taxAbove72000 = 0.25 * (income - 72000);
-        return Math.min(750, 600 + taxAbove72000);
-    } else {
-        const taxAbove200000 = 0.25 * (income - 200000);
-        return 750 + taxAbove200000;
-    }
+  if (income <= 20000) {
+    return 0;
+  } else if (income > 20000 && income <= 36000) {
+    return Math.min(300, 0.06 * (income - 20000));
+  } else if (income > 36000 && income <= 48000) {
+    const taxAbove36000 = 0.06 * (income - 36000);
+    return Math.min(450, 300 + taxAbove36000);
+  } else if (income > 48000 && income <= 72000) {
+    const taxAbove48000 = 0.25 * (income - 48000);
+    return Math.min(600, 450 + taxAbove48000);
+  } else if (income > 72000 && income <= 200000) {
+    const taxAbove72000 = 0.25 * (income - 72000);
+    return Math.min(750, 600 + taxAbove72000);
+  } else {
+    const taxAbove200000 = 0.25 * (income - 200000);
+    return Math.min(750 + taxAbove200000, 900);
+  }
 }
 
 export function calculateAlbertaTax(income: number): number {
